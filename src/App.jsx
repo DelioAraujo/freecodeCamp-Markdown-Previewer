bc4f7b18b7736b87723d8c4afab1355ebb30473e
DelioAraujo <delioaraujo@gmail.com>
2023-10-13 18:40:01 -0300

    passando até o 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import ReactMarkdown from 'react-markdown'
+
+const inicialState = `# Header 1
+## Sub Header 2
+[Link](https://www.example.com)
+\`inline code\`
+\`\`\`
+// Code block
+const code = "Hello, world!";
+\`\`\`
+- List item 1
+- List item 2
+> Blockquote
+![Image](https://via.placeholder.com/150)
+**Bolded Text**`
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState(inicialState)
+
+  // generic handleChange function
+  const handleChange = (e) => {
+    const newText = e.target.value;
+    setText(newText);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <p>Editor</p>
+    <textarea value={text} id='editor' onChange={handleChange}></textarea>
+    <p>Previewer</p>
+    <div id='preview'>
+        <ReactMarkdown>{text}</ReactMarkdown>
+    </div>
     </>
   )
 }
